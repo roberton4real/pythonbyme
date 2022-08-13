fd0ec6392964b15d73fcb50622b1c90f6bdbc1a9
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-13 14:22:42 -0600

    putting restriction on my code

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ calculation_to_units = 24
 name_of_unit = "hours"
 
 def days_to_units(num_of_days):
-    condition_check = num_of_days > 0
-    print(type(condition_check))
+    #condition_check = num_of_days > 0
+    #print(type(condition_check))
 
     if num_of_days > 0:
         return(f"{num_of_days} days are  {num_of_days * calculation_to_units} {name_of_unit}")
@@ -13,10 +13,13 @@ def days_to_units(num_of_days):
         return "you entered a negative value, no conversion value"
 
 user_input = input("Hey user, enter a number of days and i will convert it to hours!\n")
-user_input_number = int(user_input)
+if user_input.isdigit():
+    user_input_number = int(user_input)
+    calculated_value = days_to_units(int(user_input))
+    print(calculated_value)
+else:
+    print("your input is not a number. Don't ruin my program")
 
-calculated_value = days_to_units(int(user_input))
-print(calculated_value) #aug 13
 
 
 
