8a17481cea06e6ff1d3ff49af51550f4f4a1d385
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-13 13:57:59 -0600

    python codes

---
+++ test.py
@@ -0,0 +1,23 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+    condition_check = num_of_days > 0
+    print(type(condition_check))
+
+    if num_of_days > 0:
+        return(f"{num_of_days} days are  {num_of_days * calculation_to_units} {name_of_unit}")
+    elif num_of_days == 0:
+        return "you entered a number that is 0 days, no conversion value"
+    else:
+        return "you entered a negative value, no conversion value"
+
+user_input = input("Hey user, enter a number of days and i will convert it to hours!\n")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(int(user_input))
+print(calculated_value)
+
+
+
+
