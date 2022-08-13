d7f8864ae84893523492da71ac24c491d57e91d9
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-13 15:06:23 -0600

    2 - new change clean up code

--- test.py
+++ test.py
@@ -2,28 +2,23 @@ calculation_to_units = 24
 name_of_unit = "hours"
 
 def days_to_units(num_of_days):
-    #condition_check = num_of_days > 0
-    #print(type(condition_check))
+    return(f"{num_of_days} days are  {num_of_days * calculation_to_units} {name_of_unit}")
 
-    if num_of_days > 0:
-        return(f"{num_of_days} days are  {num_of_days * calculation_to_units} {name_of_unit}")
-    elif num_of_days == 0:
-        return "you entered a number that is 0 days, no conversion value"
-    else:
-        return "you entered a negative value, no conversion value"
 
 
 def validate_and_execute():
     if user_input.isdigit():
         user_input_number = int(user_input)
-        calculated_value = days_to_units(int(user_input))
-        print(calculated_value)
+        if user_input_number > 0:
+            calculated_value = days_to_units(int(user_input))
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a number that is 0 days, no conversion value")
     else:
         print("your input is not a number. Don't ruin my program")
 
 user_input = input("Hey user, enter a number of days and i will convert it to hours!\n")
 validate_and_execute()
-
 
 
 
