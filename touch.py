173ca219ec9ac4f00844a69db8b2a3a852060f52
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-15 07:12:52 -0600

    added funtions

--- touch.py
+++ touch.py
@@ -1,13 +1,18 @@
 # to avoid repeating the same line of use variables 
-#
-calculation_to_units = 20 * 24 * 60 
 
+calculation_to_units = 24 
+name_of_unit = "Hours"
 print(f"20 days are  {20 * 24 * 60}  minutes")
 print(f"20 days are  {21 * 24 * 60}  minutes")
 print(f"20 days are  {22 * 24 * 60}  minutes")
 print(f"20 days are  {23 * 24 * 60}  minutes") 
 
 # now this can be written as 
-print(f"20 days are  {calculation_to_units}  minutes")
+
+print(f"20 days are  {20 * calculation_to_units}  {name_of_unit}")
+
+# using functions
+
+
 
 
