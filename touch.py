ce469bf6e9d81f30f7da574cc67d98ff3e1c585c
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-15 07:52:38 -0600

    python notes

--- touch.py
+++ touch.py
@@ -1,4 +1,8 @@
-# to avoid repeating the same line of use variables 
+# print("20 days are " + str(50 )+ " minutes") # string concatenation combine string using plus sign
+# # + str(50 )+ => string concatenation
+# # + str(50 )+ => string concatenation
+# print(f"20 days are {50} minutes") #way cooler way then using the plus sign
+# # to avoid repeating the same line of use variables 
 
 calculation_to_units = 24 
 name_of_unit = "Hours"
@@ -14,12 +18,10 @@ print(f"20 days are  {20 * calculation_to_units}  {name_of_unit}")
 # using functions - block of code 
 # that does something more complex used to avoid repeating the same logic in you code 
 
-def days_to_units(num_of_days): #this line defines a function
+def days_to_units(num_of_days, custom_message): #this line defines a function
     # num_of_days is a variable in my def it's called a parameter 
-    print(f"20 days are  {20 * calculation_to_units}  {name_of_unit}")
-    print("All Good")
+    print(f"{num_of_days} {20 * calculation_to_units}  {name_of_unit}")
+    print("custom_message")
 
-days_to_units(35) #this is needed to execute my functions
-days_to_units(45)
-days_to_units(350)
-days_to_units(3125)
+days_to_units(35, "All great") #this is needed to execute my functions
+days_to_units(45, "Looks great")
