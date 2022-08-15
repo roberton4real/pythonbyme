694d1ab5984e8705069edcad4884063ba731e72a
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-15 06:38:49 -0600

    string concatenation

--- touch.py
+++ touch.py
@@ -1,2 +1,2 @@
-print("200 is a great number")
-print(2) #data type could be str (mama) , bool (true or false < >) , float(2.9), Int (5) 
+print("20 days are " + str(50 )+ " minutes") # string concatenation combine string using plus sign
+# + str(50 )+ => string concatenation
