d67b8578d719548a1d7948e582e5f43e11cf4802
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-15 06:40:03 -0600

    better way of writting the string concatenation

--- touch.py
+++ touch.py
@@ -1,2 +1,3 @@
 print("20 days are " + str(50 )+ " minutes") # string concatenation combine string using plus sign
 # + str(50 )+ => string concatenation
+print(f"20 days are {50} minutes") #way cooler way then using the plus sign
