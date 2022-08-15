b99b7430b49726864ef6e75fb4071787bd0fe753
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-15 07:29:08 -0600

    parameter

--- touch.py
+++ touch.py
@@ -14,10 +14,11 @@ print(f"20 days are  {20 * calculation_to_units}  {name_of_unit}")
 # using functions - block of code 
 # that does something more complex used to avoid repeating the same logic in you code 
 
-def days_to_units(): #this line defines a function
+def days_to_units(num_of_days): #this line defines a function
+    # num_of_days is a variable in my def it's called a parameter 
     print(f"20 days are  {20 * calculation_to_units}  {name_of_unit}")
     print("All Good")
 
-days_to_units() #this is needed to execute my functions
+days_to_units(35) #this is needed to execute my functions
 
 
