de26c9c14a07c5670c132063c6031442c70c47f3
N3WJOHN <90915572+roberton4real@users.noreply.github.com>
2022-08-15 07:02:58 -0600

    variables

--- touch.py
+++ touch.py
@@ -1,3 +1,13 @@
-print("20 days are " + str(50 )+ " minutes") # string concatenation combine string using plus sign
-# + str(50 )+ => string concatenation
-print(f"20 days are {50} minutes") #way cooler way then using the plus sign
+# to avoid repeating the same line of use variables 
+#
+calculation_to_units = 20 * 24 * 60 
+
+print(f"20 days are  {20 * 24 * 60}  minutes")
+print(f"20 days are  {21 * 24 * 60}  minutes")
+print(f"20 days are  {22 * 24 * 60}  minutes")
+print(f"20 days are  {23 * 24 * 60}  minutes") 
+
+# now this can be written as 
+print(f"20 days are  {calculation_to_units}  minutes")
+
+
